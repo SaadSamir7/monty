e52452060fe669078af321d882701b5ef7111d18
SaadSamir7 <saadsamir7177@gmail.com>
2023-10-20 01:49:09 +0300

    fix errors

--- 14_handle_rotl.c
+++ 14_handle_rotl.c
@@ -1,28 +1,33 @@
 #include "monty.h"
 
 /**
- * handle_rotl - rotates the stack to the top
+ * handle_rotl - rotates the stack to the top.
  * @head: stack head
  * @counter: line_number
  * Return: no return
  */
 void handle_rotl(stack_t **head, unsigned int counter)
 {
-	stack_t *new_head = (*head)->next;
-	stack_t *last_node = *head;
+	stack_t *top, *second, *last;
+
 	(void)counter;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return;
-
-	while (last_node->next != NULL)
+	if (*head && (*head)->next)
 	{
-		last_node = last_node->next;
-	}
+		top = *head;
+		second = top->next;
+		last = top;
 
-	last_node->next = *head;
-	(*head)->prev = last_node;
-	(*head)->next = NULL;
-	new_head->prev = NULL;
-	*head = new_head;
+		while (last->next)
+		{
+			last = last->next;
+		}
+
+		*head = second;
+		second->prev = NULL;
+
+		top->next = NULL;
+		top->prev = last;
+		last->next = top;
+	}
 }
